81b1743c623bdd3a5adeaaa5820b027c075f2ce9
picocodes <picocodes@gmail.com>
2023-04-21 11:03:24 +0300

    fix deprecated property declarations

--- src/REST_Versions.php
+++ src/REST_Versions.php
@@ -16,6 +16,20 @@ defined( 'ABSPATH' ) || exit;
 class REST_Versions {
 
 	/**
+	 * The namespace for the REST API.
+	 *
+	 * @var string
+	 */
+	protected $namespace;
+
+	/**
+	 * The base for the REST API.
+	 *
+	 * @var string
+	 */
+	protected $rest_base;
+
+	/**
 	 * Loads the class.
 	 *
 	 */
